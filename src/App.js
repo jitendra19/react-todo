a0de2ae431b635aea067f313f2b77f3ccca13a1e
jitendra singhal <jiten.cs2010@gmail.com>
2021-11-12 03:46:19 +0530

    more functionality on todo

--- src/App.js
+++ src/App.js
@@ -1,44 +1,60 @@
 import React from "react";
-
 import "./styles.css";
-
 const todoData = [
   {
     id: "1",
-    title: "abc",
+    title: "jiten",
     done: false
   },
   {
     id: "2",
-    title: "abc",
+    title: "pinku",
     done: true
   },
   {
     id: "3",
-    title: "abc",
+    title: "JP",
     done: false
   },
   {
     id: "4",
-    title: "abc",
+    title: "PJ",
     done: true
   }
 ];
-
 export default function App() {
   const [list1, setlist1] = React.useState(todoData);
+  const newValueRef = React.useRef(null);
   const togglecheckbox = (item) => {
     const index = list1.findIndex((a) => a.id === item.id);
     const list2 = [...list1];
     list2[index].done = !list2[index].done;
     setlist1(list2);
   };
+  const deleteItem = (item) => {
+    const index = list1.findIndex((a) => a.id === item.id);
+    const list2 = [...list1];
+    list2.splice(index, 1);
+    setlist1(list2);
+  };
+  const addNewItem = (e) => {
+    const obj = {
+      id: list1.length + 1,
+      title: newValueRef.current.value,
+      done: false
+    };
+    setlist1([obj, ...list1]);
+    newValueRef.current.value = null;
+    e.preventDefault();
+  };
   return (
     <div className="App">
       <h1>MY TODO</h1>
+      <form style={{ margin: "5px" }} onSubmit={addNewItem}>
+        add new item : <input type="text" ref={newValueRef} />
+      </form>
       <div style={{ padding: "5px" }}>
         {list1 &&
-          list1.length &&
           list1.map((item) => {
             return (
               <div>
@@ -56,6 +72,16 @@ export default function App() {
                     {item.title}
                   </span>
                 </div>
+                <button
+                  style={{
+                    float: "right",
+                    background: "orange"
+                  }}
+                  onClick={(e) => deleteItem(item)}
+                >
+                  Del
+                  {/* Del <i class="fa fa-times"></i> */}
+                </button>
               </div>
             );
           })}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
+import "font-awesome/css/font-awesome.min.css";
 
 import App from "./App";
 
