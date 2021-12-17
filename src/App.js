15106d341d04220aa1b43f6a8b122add282145c1
jitendra singhal <jiten.cs2010@gmail.com>
2021-12-18 04:23:39 +0530

    css changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { Fragment } from "react";
+import React from "react";
 import "./styles.css";
-import TodoItem from "./TodoItem";
 const todoData = [
   {
     id: "1",
-    title: "jiten",
+    title:
+      "jiten jitendraadshljfh asdflhasdf asdlfasdhfahl  asdflhasdf asdlfasdhfahl kasdfhlagsdfh",
     done: false
   },
   {
@@ -49,24 +49,63 @@ export default function App() {
     e.preventDefault();
   };
   return (
-    <Fragment>
-      <div className="App">
-        <h1>MY TODO</h1>
-        <form style={{ margin: "5px" }} onSubmit={addNewItem}>
-          add new item : <input type="text" ref={newValueRef} />
-        </form>
-        <div style={{ padding: "5px" }}>
-          {list1 &&
-            list1.map((item) => (
-              <TodoItem
-                item={item}
-                deleteItem={deleteItem}
-                togglecheckbox={togglecheckbox}
-              />
-            ))}
-        </div>
+    <div className="App">
+      <h1>MY TODO</h1>
+      <form style={{ margin: "5px" }} onSubmit={addNewItem}>
+        add new item : <input type="text" ref={newValueRef} />
+      </form>
+      <div style={{ padding: "2px" }}>
+        {list1 &&
+          list1.map((item) => {
+            return (
+              <ul key={item.id} style={{ width: "300px" }}>
+                <div
+                  className="item"
+                  key={item.id}
+                  onClick={(e) => togglecheckbox(item)}
+                >
+                  <input
+                    style={{
+                      "margin-ight": "2rem",
+                      height: "25px",
+                      width: "25px",
+                      minWidth: "25px"
+                    }}
+                    type="checkbox"
+                    checked={!!item.done}
+                    onChange={() => {}}
+                  />
+                  <div
+                    style={{
+                      margin: "5px",
+                      minWidth: "300px",
+                      display: "inline"
+                    }}
+                  >
+                    <span
+                      style={{
+                        textDecoration: !!item.done ? "line-through" : "none"
+                      }}
+                    >
+                      {item.title}
+                    </span>
+                  </div>
+
+                  <button
+                    style={{
+                      float: "right",
+                      background: "orange"
+                    }}
+                    onClick={(e) => deleteItem(item)}
+                  >
+                    Del
+                    {/* Del <i class="fa fa-times"></i> */}
+                  </button>
+                </div>
+              </ul>
+            );
+          })}
       </div>
-      <br />
-    </Fragment>
+    </div>
   );
 }
