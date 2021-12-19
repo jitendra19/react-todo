41f81a01e4d9468a6f096fb9874c0f370e012df9
jitendra singhal <jiten.cs2010@gmail.com>
2021-12-20 00:07:40 +0530

    some more changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./styles.css";
+import OzoneComponent from "./ozone";
 const todoData = [
   {
     id: "1",
-    title:
-      "jiten jitendraadshljfh asdflhasdf asdlfasdhfahl  asdflhasdf asdlfasdhfahl kasdfhlagsdfh",
+    title: "jiten",
     done: false
   },
   {
@@ -23,8 +23,14 @@ const todoData = [
     done: true
   }
 ];
+
+const menus = [
+  { label: "Todo", id: 1 },
+  { label: "Ozone", id: 2 }
+];
 export default function App() {
   const [list1, setlist1] = React.useState(todoData);
+  const [currentMenu, SetCurrentMenu] = React.useState(2);
   const newValueRef = React.useRef(null);
   const togglecheckbox = (item) => {
     const index = list1.findIndex((a) => a.id === item.id);
@@ -48,64 +54,96 @@ export default function App() {
     newValueRef.current.value = null;
     e.preventDefault();
   };
+  const handleMenuClick = (e, a) => {
+    e.preventDefault();
+    SetCurrentMenu(a.id);
+  };
   return (
     <div className="App">
-      <h1>MY TODO</h1>
-      <form style={{ margin: "5px" }} onSubmit={addNewItem}>
-        add new item : <input type="text" ref={newValueRef} />
-      </form>
-      <div style={{ padding: "2px" }}>
-        {list1 &&
-          list1.map((item) => {
+      <div className="mainMenu">
+        {menus &&
+          menus.map((a) => {
             return (
-              <ul key={item.id} style={{ width: "300px" }}>
-                <div
-                  className="item"
-                  key={item.id}
-                  onClick={(e) => togglecheckbox(item)}
-                >
-                  <input
-                    style={{
-                      "margin-ight": "2rem",
-                      height: "25px",
-                      width: "25px",
-                      minWidth: "25px"
-                    }}
-                    type="checkbox"
-                    checked={!!item.done}
-                    onChange={() => {}}
-                  />
-                  <div
-                    style={{
-                      margin: "5px",
-                      minWidth: "300px",
-                      display: "inline"
-                    }}
-                  >
-                    <span
-                      style={{
-                        textDecoration: !!item.done ? "line-through" : "none"
-                      }}
-                    >
-                      {item.title}
-                    </span>
-                  </div>
-
-                  <button
-                    style={{
-                      float: "right",
-                      background: "orange"
-                    }}
-                    onClick={(e) => deleteItem(item)}
-                  >
-                    Del
-                    {/* Del <i class="fa fa-times"></i> */}
-                  </button>
-                </div>
-              </ul>
+              <a
+                href="/"
+                style={{ margin: "15px" }}
+                onClick={(e) => handleMenuClick(e, a)}
+              >
+                {a.label}
+              </a>
             );
           })}
       </div>
+      {currentMenu === 1 && (
+        <div className="todo">
+          <h1>MY TODO</h1>
+          <form style={{ margin: "5px" }} onSubmit={addNewItem}>
+            add new item : <input type="text" ref={newValueRef} />
+          </form>
+          <div style={{ padding: "2px" }}>
+            {list1 &&
+              list1.map((item) => {
+                return (
+                  <ul key={item.id} style={{ width: "300px" }}>
+                    <div
+                      className="item"
+                      key={item.id}
+                      onClick={(e) => togglecheckbox(item)}
+                    >
+                      <input
+                        style={{
+                          "margin-ight": "2rem",
+                          height: "25px",
+                          width: "25px",
+                          minWidth: "25px"
+                        }}
+                        type="checkbox"
+                        checked={!!item.done}
+                        onChange={() => {}}
+                      />
+                      <div
+                        style={{
+                          margin: "5px",
+                          minWidth: "300px",
+                          display: "inline"
+                        }}
+                      >
+                        <span
+                          style={{
+                            textDecoration: !!item.done
+                              ? "line-through"
+                              : "none"
+                          }}
+                        >
+                          {item.title}
+                        </span>
+                      </div>
+
+                      <button
+                        style={{
+                          float: "right",
+                          background: "orange"
+                        }}
+                        onClick={(e) => deleteItem(item)}
+                      >
+                        Del
+                        {/* Del <i class="fa fa-times"></i> */}
+                      </button>
+                    </div>
+                  </ul>
+                );
+              })}
+          </div>
+        </div>
+      )}
+      {currentMenu === 2 && (
+        <div className="ozone">
+          <h1>Ozone exercize</h1>
+          <div>
+            <OzoneComponent />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/ozone.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { ArrayField, Datagrid, DateField, UrlField } from "react-admin";
+const data = {
+  id: 123,
+  tags: [{ name: "foo" }, { name: "bar" }],
+  backlinks: [
+    {
+      uuid: "34fdf393-f449-4b04-a423-38ad02ae159e",
+      date: "2012-08-10T00:00:00.000Z",
+      url: "http://example.com/foo/bar.html"
+    },
+    {
+      uuid: "d907743a-253d-4ec1-8329-404d4c5e6cf1",
+      date: "2012-08-14T00:00:00.000Z",
+      url: "https://blog.johndoe.com/2012/08/12/foobar.html"
+    }
+  ]
+};
+export default (props) => {
+  return (
+    <ArrayField source="backlinks">
+      <Datagrid>
+        <DateField source="date" />
+        <UrlField source="url" />
+      </Datagrid>
+    </ArrayField>
+  );
+  // return <div>jtiendra</div>;
+};
